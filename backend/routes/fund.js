74b4a0efb7913b496655bff54bbbde875c43f29c
Yuttana Khumnual <yuttana@mpam.local>
2018-09-20 17:41:21 +0700

    fund backend & API modify paging not finish yet

--- backend/routes/fund.js
+++ backend/routes/fund.js
@@ -5,8 +5,8 @@ const checkAuth = require('../middleware/check-auth');
 const fundController = require('../controllers/fund')
 
 // router.get("", checkAuth, fundController.getFunds);
+// router.get("", fundController.getFunds);
 router.get("", fundController.getFunds);
-
 
 router.get("/:code", fundController.getFundByCode); //http://localhost:3000/api/fund/ACFIF2
 
