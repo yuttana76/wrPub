b67b103e82f28987249943eb77cd2adf3d465752
Yuttana Khumnual <yuttana@mpam.local>
2019-01-25 11:24:35 +0700

    Rename function SaveLoginLog to be loginProcessLog

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -16,140 +16,6 @@ const TOKEN_SECRET_STRING = dbConfig.TOKEN_SECRET_STRING;
 const TOKEN_EXPIRES = dbConfig.TOKEN_EXPIRES;
 const TOKEN_EXPIRES_SEC = 3600;
 
-// exports.userLogin = (req, res, next) => {
-
-//   let fetchedUser;
-//   let _userName = req.body.email
-//   logger.info( `API /Login - ${req.originalUrl} - ${req.ip} - ${_userName}`);
-
-//   let queryStr = `SELECT a.*
-//                         ,b.Title_Name_E + ' ' + b.First_Name_T + ' ' + b.Last_Name_T FULLNAME
-//                  FROM [MIT_USERS] a
-//                  LEFT JOiN MIT_Account_Profile b ON a.USERID = b.CUST_CODE
-//                  WHERE STATUS = 'A'  AND CURRENT_TIMESTAMP < ISNULL(EXPIRE_DATE,CURRENT_TIMESTAMP+1)
-//                  AND LoginName='${_userName}'`;
-
-//   const sql = require('mssql')
-
-//  sql.connect(config).then(pool => {
-//     // Query
-//     return pool.request()
-//     .query(queryStr)})
-//     .then(user => {
-
-//       if(!user){
-//        logger.error( `API /Login  System error - ${req.originalUrl} - ${req.ip} `);
-//        sql.close();
-
-//        const _loginCode ='101';
-
-//         return res.status(401).json({
-//           MSG_CODE: _loginCode,
-//           MSG_DESC: getLogiMsg(_loginCode)
-//         });
-//       } else {
-//          sql.close();
-//          fetchedUser = user;
-//          return bcrypt.compare(req.body.password,user.recordset[0].PASSWD);
-//       }
-
-//     })
-//     .then(result =>{
-
-//       if(!result){
-//       // ***** INCORRECT PWD.
-//         let _loginCode ='101';
-
-//         // Save login fail log
-//         saveLoginLog(_userName,_loginCode,req.ip,req.originalUrl)
-//         .then(function(data) {
-
-//           // Check was user lock ?
-//           // If user lock will return user lock message.
-//           if(data.UserWasLock ==='Y'){
-//             _loginCode = '102';
-
-//           }
-
-//           return res.status(401).json({
-//             MSG_CODE: _loginCode,
-//             MSG_DESC: getLogiMsg(_loginCode)
-//           });
-
-//         }).catch(function(err) {
-//           console.log("It failed!", err);
-//         })
-
-//       }else{
-//         // CORRECT PWD
-//         let _loginCode ='000';
-
-//           // Check was user lock ?
-//           // If user locked return login fail
-//           if(fetchedUser.recordset[0].userLock ==='Y'){
-//             _loginCode ='102';
-//             return res.status(401).json({
-//               MSG_CODE: _loginCode,
-//               MSG_DESC: getLogiMsg(_loginCode)
-//             });
-
-//           }
-
-//           // Save login success log
-//         saveLoginLog(_userName,_loginCode,req.ip,req.originalUrl)
-//         .then(function(data) {
-
-//                 //Generate token
-//               const token = jwt.sign(
-//                 {USERID: fetchedUser.recordset[0].USERID},
-//                 TOKEN_SECRET_STRING,
-//                 { expiresIn: TOKEN_EXPIRES},
-//               );
-
-//               res.status(200).json({
-//                 token: token,
-//                 expiresIn: TOKEN_EXPIRES_SEC,//3600 = 1h
-//                 userData: fetchedUser.recordset[0].USERID,
-//                 LoginName: fetchedUser.recordset[0].LoginName,
-//                 USERID: fetchedUser.recordset[0].USERID,
-//                 FULLNAME: fetchedUser.recordset[0].FULLNAME,
-//                 MSG_CODE: _loginCode,
-//                 MSG_DESC: getLogiMsg(_loginCode)
-//               });
-//         });
-
-//       }
-
-//       sql.close();
-//     })
-//     .catch(err => {
-//         // NOT FOUND USER
-//         logger.info( `API /Login Auth failed by no user - ${_userName} -${err}`);
-//         sql.close();
-
-//         const _loginCode ='101';
-
-//         return res.status(401).json({
-//           MSG_CODE: _loginCode,
-//           MSG_DESC: getLogiMsg(_loginCode)
-//         });
-//     })
-
-//   sql.on("error", err => {
-//    err.message
-//     sql.close();
-//     logger.error( `API /Login error - ${req.originalUrl} - ${req.ip} - ${err} `);
-
-//     const _loginCode ='101';
-//     return res.status(401).json({
-//       MSG_CODE: _loginCode,
-//       MSG_DESC: getLogiMsg(_loginCode)
-//     });
-
-//   });
-//  }
-
-
 exports.userLoginByParam = (req, res, next) => {
 
   let fetchedUser;
@@ -196,7 +62,7 @@ exports.userLoginByParam = (req, res, next) => {
         let _loginCode ='101';
 
         // Save login fail log
-        saveLoginLog(_userName,_loginCode,req.ip,req.originalUrl)
+        loginProcessLog(_userName,_loginCode,req.ip,req.originalUrl)
         .then(function(data) {
 
           // Check was user lock ?
@@ -229,7 +95,7 @@ exports.userLoginByParam = (req, res, next) => {
           }
 
           // Save login success log
-        saveLoginLog(_userName,_loginCode,req.ip,req.originalUrl)
+          loginProcessLog(_userName,_loginCode,req.ip,req.originalUrl)
         .then(function(data) {
 
           console.log(' ***** data >> ' +JSON.stringify(data));
@@ -351,6 +217,7 @@ exports.resetPassword = (req,res,next)=>{
   var input_userName = req.body.LoginName;
   var newPassword = req.body.newPassword;
   var idCard = req.body.id;
+  var RESET_PWD_CODE = '901';
 
   bcrypt.hash(newPassword, SALT_WORK_FACTOR)
   .then(hash =>{
@@ -394,12 +261,14 @@ exports.resetPassword = (req,res,next)=>{
         .input('input_userName', sql.VarChar(50), input_userName)
         .input('idCard', sql.VarChar(50), idCard)
         .query(queryStr, (err, result) => {
-            // ... error checks
             if(err){
               res.status(500).json({
                 errMsg:err
               });
             }else {
+
+              // saveLoginLog(input_userName,RESET_PWD_CODE,req.ip,req.originalUrl)
+
               res.status(200).json({
                 MSG_CODE: result.recordsets[0][0].msg_code,
                 MSG_DESC: getLogiMsg(result.recordsets[0][0].msg_code)
@@ -505,9 +374,8 @@ function getLogiMsg(_code){
 }
 
 
-function saveLoginLog(_userName,_loginCode,_ip,_url) {
+function loginProcessLog(_userName,_loginCode,_ip,_url) {
 
-    // console.log(`call function saveLoginLog ${_userName} -  ${_loginCode} - ${_ip} - ${_url}`);
     const LOGIN_FAIL_LOCK_NO = wr_properties.LOGIN_FAIL_LOCK_NO;
     return new Promise(function(resolve, reject) {
     // const _NO =1;
