9cac7b7f9660f5bfffc73d6d959f08fa898e3cda
Apple <apple@Apples-MacBook-Pro.local>
2019-04-25 21:01:22 +0700

    Update https

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const app = require("./app");
 const debug = require("debug")("node-angular");
 const http = require("http");
+const https = require('https');
+const fs = require('fs');
 
 
 const normalizePort = val => {
@@ -47,7 +49,54 @@ const onListening = () => {
 const port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
 
-const server = http.createServer(app);
-server.on("error", onError);
-server.on("listening", onListening);
-server.listen(port);
+//HTTP
+// const server = http.createServer(app);
+// server.on("error", onError);
+// server.on("listening", onListening);
+// server.listen(port);
+
+
+//HTTPS
+console.log('DIR>' + __dirname);
+// ************************************** GET IP address
+var os = require('os');
+var ifaces = os.networkInterfaces();
+
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      console.log(ifname + ':' + alias, iface.address);
+    } else {
+      // this interface has only one ipv4 adress
+      console.log(ifname, iface.address);
+    }
+    ++alias;
+  });
+
+});
+
+// var intermediateCertificate = fs.readFileSync('intermediate.pem', 'utf8');
+// https.createServer({
+//     key: privateKey,
+//     cert: certificate,
+//     ca: [ intermediateCertificate ]
+// }, app).listen(port);
+
+// ************************************** HTTPS
+const option = {
+  key: fs.readFileSync(__dirname+'/ca/key.pem'),
+  cert: fs.readFileSync(__dirname+'/ca/cert.pem'),
+    passphrase: 'mpam@2019'
+};
+var server = https.createServer(option, app)
+.listen(port,function () {
+  console.log("Listening on port https://localhost:%s", server.address().port);
+})
