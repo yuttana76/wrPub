f2b7dd3473a8da3f89da273d3b4433f3db827d61
Yuttana Khumnual <yuttana@mpam.local>
2019-05-14 09:11:52 +0700

    Config to prod

--- backend/config/db-config.js
+++ backend/config/db-config.js
@@ -1,8 +1,8 @@
 exports.dbParameters = {
   user: 'mftsuser',//process.env.AUTH_SRV_USER,
   password: 'P@ssw0rd',//process.env.AUTH_SRV_PWD,
-  server: '192.168.10.48', //Development
-  // server: '192.168.2.25', // Prod
+  // server: '192.168.10.48', //Development
+  server: '192.168.2.25', // Prod
   database: 'MFTS',// WR_MFTS
   pool: {
     max: 10,
